2d36c9d087ae633540186028abd4ce64400b8ff8
shi <shi_19890905@163.com>
2021-02-03 11:14:48 +0800

    shi #0203

--- src/views/sections/mv.vue
+++ src/views/sections/mv.vue
@@ -4,24 +4,126 @@
       src="./111.png"
       alt="111.png"
     >
+    <video-player
+      class="video-player vjs-custom-skin"
+      ref="videoPlayer"
+      :options="playerOptions"
+      :playsinline="true"
+      @play="onPlayerPlay($event)"
+      @pause="onPlayerPause($event)"
+      @ended="onPlayerEnded($event)"
+      @loadeddata="onPlayerLoadeddata($event)"
+      @waiting="onPlayerWaiting($event)"
+      @playing="onPlayerPlaying($event)"
+      @timeupdate="onPlayerTimeupdate($event)"
+      @canplay="onPlayerCanplay($event)"
+      @canplaythrough="onPlayerCanplaythrough($event)"
+      @ready="playerReadied"
+      @statechanged="playerStateChanged($event)"
+    />
     <div />
   </div>
 </template>
 <script>
-
+import { videoPlayer } from 'vue-video-player'
+import 'video.js/dist/video-js.css'
 export default {
   name: 'Mv',
+  components: {
+    videoPlayer
+  },
   data () {
     return {
+      // videojs options
+      playerOptions: {
+        playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
+        autoplay: false, // 如果为true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 是否视频一结束就重新开始。
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true,
+        notSupportedMessage: '此视频暂无法播放，请稍后再试',
+        sources: [
+          {
+            type: 'video/mp4',
+            // mp4
+            src: 'http://vjs.zencdn.net/v/oceans.mp4'
+            // webm
+            // src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+          }
+        ]
+        // poster: require('./111.png')
+      }
+    }
+  },
+  mounted () {
+    // console.log('this is current player instance object', this.player)
+    setTimeout(() => {
+      console.log('dynamic change options', this.player)
+      // change src
+      // this.playerOptions.sources[0].src = 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm';
+      // change item
+      // this.$set(this.playerOptions.sources, 0, {
+      //   type: "video/mp4",
+      //   src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm',
+      // })
+      // change array
+      // this.playerOptions.sources = [{
+      //   type: "video/mp4",
+      //   src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm',
+      // }]
+      this.player.muted(false)
+    }, 5000)
+  },
+  computed: {
+    player () {
+      return this.$refs.videoPlayer.player
     }
   },
   methods: {
+    // listen event
+    onPlayerPlay (player) {
+      // console.log('player play!', player)
+    },
+    onPlayerPause (player) {
+      // console.log('player pause!', player)
+    },
+    onPlayerEnded (player) {
+      // console.log('player ended!', player)
+    },
+    onPlayerLoadeddata (player) {
+      // console.log('player Loadeddata!', player)
+    },
+    onPlayerWaiting (player) {
+      // console.log('player Waiting!', player)
+    },
+    onPlayerPlaying (player) {
+      // console.log('player Playing!', player)
+    },
+    onPlayerTimeupdate (player) {
+      // console.log('player Timeupdate!', player.currentTime())
+    },
+    onPlayerCanplay (player) {
+      // console.log('player Canplay!', player)
+    },
+    onPlayerCanplaythrough (player) {
+      // console.log('player Canplaythrough!', player)
+    },
+    // or listen state event
+    playerStateChanged (playerCurrentState) {
+      // console.log('player current update state', playerCurrentState)
+    },
+    // player is ready
+    playerReadied (player) {
+      // seek to 10s
+      console.log('example player 1 readied', player)
+      player.currentTime(10)
+      // console.log('example 01: the player is readied', player)
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.test{
-  width: 100%;
-  height: 100%;
-}
 </style>
